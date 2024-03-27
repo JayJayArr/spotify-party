df60e88421bed892090fdd795550845fb48001e3
JayJayArr <JayArr@outlook.de>
2024-03-27 09:36:11 +0100

    add SIGTERM

--- spotify-party-bcknd/src/main.ts
+++ spotify-party-bcknd/src/main.ts
@@ -2,12 +2,14 @@ import * as compression from 'compression';
 
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
-import helmet from 'helmet';
+
+//import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   //app.use(helmet());
+  app.enableShutdownHooks();
   app.use(compression());
   await app.listen(3000);
 }
--- spotify-party/src/environments/environment.prod.ts
+++ spotify-party/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     name: '',
-    BackendAddress: '192.168.0.145',
+    BackendAddress: '192.168.0.200',
     BackendPort: '3000',
 };
