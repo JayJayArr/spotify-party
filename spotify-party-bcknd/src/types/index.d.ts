79e2c117a8b636697015c16b05cae5a97f3d1b36
JayJayArr <JayArr@outlook.de>
2025-04-17 10:23:49 +0200

    move unoxidized to separate branch

--- spotify-party-bcknd/src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-  var authcode: string;
-  var token: string;
-  var refresh_token: string;
-  var token_expires;
-}
-
-export {};
--- spotify-party/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    name: '',
-    BackendAddress: '192.168.0.200',
-    BackendPort: '3000',
-};
