c993f591eafa8209361497268c6f3086a25c132d
JayJayArr <JayArr@outlook.de>
2025-04-20 15:40:50 +0200

    home icons

--- spotoxide-ui/src/app/currentplay/currentplay.component.ts
+++ spotoxide-ui/src/app/currentplay/currentplay.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { PlaylistComponent } from '../playlist/playlist.component';
 import { Song } from '../../types';
 import { SongsService } from '../songs.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-currentplay',
-  imports: [PlaylistComponent],
+  imports: [PlaylistComponent, MatIconModule],
   templateUrl: './currentplay.component.html',
   styleUrl: './currentplay.component.scss',
 })
